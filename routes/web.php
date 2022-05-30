475587d689fc171a94b1e377fdc8640d43151c0a
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-05-30 23:20:41 +0200

    correcting windows-app download file3

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('/privacy', function () {
 });
 
 Route::get('windows-app', function () {
-    return Storage::download('public/tabwafrenchsetup.exe');
+    return Storage::download('/public/tabwafrenchsetup.exe');
 });
 
 Route::get('/', function () {
